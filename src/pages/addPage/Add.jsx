7d47f4613d7b11dd6db83c00d0ceb6a74b7124eb
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-05-25 07:24:35 +0100

    Initial commit

--- src/pages/addPage/Add.jsx
+++ src/pages/addPage/Add.jsx
@@ -7,13 +7,15 @@ const Add = () => {
     return (
         <div className="addContainer">
             <div className="addCard">
+                <img src="https://i.pinimg.com/564x/21/8c/bc/218cbcd410d9a930e1c03bb97bef10ba.jpg" alt=""
+                     className="addImg"/>
                 <label htmlFor="fileInput" className="upload">
                     <i className="fa-solid fa-upload"></i>
                     Upload an image
                 </label>
                 <input type="file" id="fileInput" className="newFile" style={{display: "none"}}/>
-                <input type="text" className="newInputTitle" placeholder="New post title here..."/>
-                <SunEditor className="sunEditor"/>
+                {/*<input type="text" className="newInputTitle" placeholder="New post title here..."/>*/}
+                <SunEditor placeholder="Create new post title and body here..." className="sunEditor"/>
                 <button className="addBtn">publish</button>
             </div>
         </div>
