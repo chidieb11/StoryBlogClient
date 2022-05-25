2ae9e6fa688a856f2571b02f5799681ba929be25
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-05-25 16:09:19 +0100

    Add UIs for Login and Register

--- src/pages/login /Login.jsx
+++ src/pages/login /Login.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import './login.css'
+import "./login.css";
+import Navbar from "../../components/navbar/Navbar";
 
 const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <Navbar/>
+            <div className="loginContainer">
+                <div className="loginCard">
+                    <form className="loginForm">
+                        <input type="text" placeholder="username" className="loginUsername"/>
+                        <input type="password" placeholder="password" className="loginPassword"/>
+                        <button className="loginBtn">login</button>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import "./register.css";
+import Navbar from "../../components/navbar/Navbar";
 
 const Register = () => {
     return (
-        <div>
-            Register
-        </div>
+        <>
+            <Navbar/>
+            <div className="registrationContainer">
+                <div className="regCard">
+                    <form className="regForm">
+                        <input type="text" placeholder="name" className="regName"/>
+                        <input type="text" placeholder="username" className="regUsername"/>
+                        <input type="password" placeholder="password" className="regPassword"/>
+                        <input type="email" placeholder="email" className="regEmail"/>
+                        <button className="regBtn">register</button>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
 
