b802792a83edee71ccc555a1532b5ba77c6b2ad2
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-05-25 17:19:59 +0100

    Auth UI commit

--- src/pages/login /Login.jsx
+++ src/pages/login /Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./login.css";
 import Navbar from "../../components/navbar/Navbar";
+import {Link} from "react-router-dom";
 
 const Login = () => {
     return (
@@ -11,6 +12,12 @@ const Login = () => {
                     <form className="loginForm">
                         <input type="text" placeholder="username" className="loginUsername"/>
                         <input type="password" placeholder="password" className="loginPassword"/>
+                        <p>
+                            Don't have an account? Register
+                            <Link to="/register">
+                                here
+                            </Link>
+                        </p>
                         <button className="loginBtn">login</button>
                     </form>
                 </div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./register.css";
 import Navbar from "../../components/navbar/Navbar";
+import {Link} from "react-router-dom";
 
 const Register = () => {
     return (
@@ -13,6 +14,12 @@ const Register = () => {
                         <input type="text" placeholder="username" className="regUsername"/>
                         <input type="password" placeholder="password" className="regPassword"/>
                         <input type="email" placeholder="email" className="regEmail"/>
+                        <p>
+                            Already have an account? Login
+                            <Link to="/login">
+                                here
+                            </Link>
+                        </p>
                         <button className="regBtn">register</button>
                     </form>
                 </div>
