6e2b6c28685065c0d5fb8283880a2ef458247068
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-05-26 09:15:48 +0100

    Initial commit

--- src/pages/addPage/Add.jsx
+++ src/pages/addPage/Add.jsx
@@ -10,16 +10,18 @@ const Add = () => {
             <Navbar/>
             <div className="addContainer">
                 <div className="addCard">
-                    <img src="https://i.pinimg.com/564x/21/8c/bc/218cbcd410d9a930e1c03bb97bef10ba.jpg" alt=""
-                         className="addImg"/>
-                    <label htmlFor="fileInput" className="upload">
-                        <i className="fa-solid fa-upload"></i>
-                        Upload an image
-                    </label>
-                    <input type="file" id="fileInput" className="newFile" style={{display: "none"}}/>
-                    {/*<input type="text" className="newInputTitle" placeholder="New post title here..."/>*/}
-                    <SunEditor placeholder="Create new post title and body here..." className="sunEditor"/>
-                    <button className="addBtn">publish</button>
+                    <form>
+                        <img src="https://i.pinimg.com/564x/21/8c/bc/218cbcd410d9a930e1c03bb97bef10ba.jpg" alt=""
+                             className="addImg"/>
+                        <label htmlFor="fileInput" className="upload">
+                            <i className="fa-solid fa-upload"></i>
+                            Upload an image
+                        </label>
+                        <input type="file" id="fileInput" className="newFile" style={{display: "none"}}/>
+                        {/*<input type="text" className="newInputTitle" placeholder="New post title here..."/>*/}
+                        <SunEditor placeholder="Create new post title and body here..." className="sunEditor"/>
+                        <button className="addBtn">publish</button>
+                    </form>
                 </div>
             </div>
         </>
