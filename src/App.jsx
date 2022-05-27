fa3cbe7d2f530216de6de66937f40601805d3143
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-05-27 20:21:54 +0100

    User settings page

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import Single from "./pages/single/Single";
 import Add from "./pages/addPage/Add";
 import Login from "./pages/login /Login";
 import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
 
 function App() {
     return (
         <div>
             <Routes>
                 <Route exact path="/" element={<Home/>}/>
-                <Route path='single' element={<Single/>}/>
-                <Route path='new' element={<Add/>}/>
-                <Route path='login' element={<Login/>}/>
-                <Route path='register' element={<Register/>}/>
+                <Route path="single" element={<Single/>}/>
+                <Route path="new" element={<Add/>}/>
+                <Route path="login" element={<Login/>}/>
+                <Route path="register" element={<Register/>}/>
+                <Route path="settings" element={<Settings/>}/>
             </Routes>
         </div>
     );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,43 +3,45 @@ import "./navbar.css";
 import {Link} from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>
-      <div className="navContainer">
-        <div className="nav">
-          <div className="navMain">
-            <div className="leftNav">
-              <Link to='/'>
-                <div className="logo">📰</div>
-              </Link>
-              <div className="searchDiv">
-                <i class="fa-solid fa-magnifying-glass"></i>
-                <input type="text" placeholder="Search" className="search" />
-              </div>
+    return (
+        <div>
+            <div className="navContainer">
+                <div className="nav">
+                    <div className="navMain">
+                        <div className="leftNav">
+                            <Link to="/">
+                                <div className="logo">📰</div>
+                            </Link>
+                            <div className="searchDiv">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                                <input type="text" placeholder="Search" className="search"/>
+                            </div>
+                        </div>
+                        <div className="rightNav">
+                            <Link to="/login">
+                                <button className="navLogin">login</button>
+                            </Link>
+                            <Link to="/register">
+                                <button className="navRegister">create account</button>
+                            </Link>
+                            <Link to="/new">
+                                <button className="navBtn">create post</button>
+                            </Link>
+                            <Link to="/settings">
+                                <span className="navImgSpan">
+                                <img
+                                    className="navImg"
+                                    src="https://i.pinimg.com/736x/67/19/b9/6719b92479b6814464fd83a117a9a243.jpg"
+                                    alt=""
+                                />
+                              </span>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="rightNav">
-              <Link to='/login'>
-                <button className="navLogin">login</button>
-              </Link>
-              <Link to='/register'>
-                <button className="navRegister">create account</button>
-              </Link>
-              <Link to='/new'>
-                <button className="navBtn">create post</button>
-              </Link>
-              <span className="navImgSpan">
-                <img
-                  className="navImg"
-                  src="https://i.pinimg.com/736x/67/19/b9/6719b92479b6814464fd83a117a9a243.jpg"
-                  alt=""
-                />
-              </span>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Navbar;
