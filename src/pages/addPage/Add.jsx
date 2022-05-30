0bac0ed22685b668d83390664f59d409f1628256
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-05-30 09:56:40 +0100

    Initial commit

--- src/pages/addPage/Add.jsx
+++ src/pages/addPage/Add.jsx
@@ -5,27 +5,39 @@ import "suneditor/dist/css/suneditor.min.css";
 import Navbar from "../../components/navbar/Navbar"; // Import Sun Editor's CSS File
 
 const Add = () => {
-    return (
-        <>
-            <Navbar/>
-            <div className="addContainer">
-                <div className="addCard">
-                    <form>
-                        <img src="https://i.pinimg.com/564x/21/8c/bc/218cbcd410d9a930e1c03bb97bef10ba.jpg" alt=""
-                             className="addImg"/>
-                        <label htmlFor="fileInput" className="upload">
-                            <i className="fa-solid fa-upload"></i>
-                            Upload an image
-                        </label>
-                        <input type="file" id="fileInput" className="newFile" style={{display: "none"}}/>
-                        {/*<input type="text" className="newInputTitle" placeholder="New post title here..."/>*/}
-                        <SunEditor placeholder="Create new post title and body here..." className="sunEditor"/>
-                        <button className="addBtn">publish</button>
-                    </form>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="addContainer">
+        <div className="addCard">
+          <form>
+            <img
+              src="https://i.pinimg.com/564x/21/8c/bc/218cbcd410d9a930e1c03bb97bef10ba.jpg"
+              alt=""
+              className="addImg"
+            />
+            <label htmlFor="fileInput" className="upload">
+              <i className="fa-solid fa-upload"></i>
+              Upload an image
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              className="newFile"
+              style={{ display: "none" }}
+              accept="image/*"
+            />
+            {/*<input type="text" className="newInputTitle" placeholder="New post title here..."/>*/}
+            <SunEditor
+              placeholder="Create new post title and body here..."
+              className="sunEditor"
+            />
+            <button className="addBtn">publish</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Add;
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -3,32 +3,45 @@ import "./settings.css";
 import Navbar from "../../components/navbar/Navbar";
 
 const Settings = () => {
-    return (
-        <div>
-            <Navbar/>
-            <div className="container">
-                <div className="settingsCard">
-                    <label className="user" htmlFor="user">update your information</label>
-                    <form className="settingsForm" action="">
-                        <input type="text" placeholder="name" className="settingsName"/>
-                        <input type="email" placeholder="email" className="settingsEmail"/>
-                        <input type="text" placeholder="username" className="settingUsername"/>
-                    </form>
-                    <span className="settingsSpan">
-                        <img
-                            src="https://i.pinimg.com/736x/51/8f/69/518f692f8d06d20fb1fe4d09804714c4.jpg"
-                            alt="" className="settingsImg"/>
-                        <label htmlFor="picUpdate" id="user">
-                            <i className="fa-solid fa-upload"></i>
-                            update profile picture
-                        </label>
-                        <input type="file" id="picUpdate" className="profilePic" style={{display: "none"}}/>
-                    </span>
-                    <button className="settingsBtn">save changes</button>
-                </div>
-            </div>
+  return (
+    <div>
+      <Navbar />
+      <div className="container">
+        <div className="settingsCard">
+          <label className="user" htmlFor="user">
+            update your information
+          </label>
+          <form className="settingsForm" action="">
+            <input type="text" placeholder="name" className="settingsName" />
+            <input type="email" placeholder="email" className="settingsEmail" />
+            <input
+              type="text"
+              placeholder="username"
+              className="settingUsername"
+            />
+          </form>
+          <span className="settingsSpan">
+            <img
+              src="https://i.pinimg.com/736x/51/8f/69/518f692f8d06d20fb1fe4d09804714c4.jpg"
+              alt=""
+              className="settingsImg"
+            />
+            <label htmlFor="picUpdate" id="user">
+              <i className="fa-solid fa-upload"></i>
+              update profile picture
+            </label>
+            <input
+              type="file"
+              id="picUpdate"
+              className="profilePic"
+              style={{ display: "none" }}
+            />
+          </span>
+          <button className="settingsBtn">save changes</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
